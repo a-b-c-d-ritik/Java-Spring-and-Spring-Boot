0a254c500d19b650a509f196dfeaf1af7037398d
Ritik Kumar <143861433+a-b-c-d-ritik@users.noreply.github.com>
2025-07-03 23:11:38 +0530

    03/07/25

--- SpringBootweb1/SpringBootweb1/src/main/java/com/example/SpringBootweb1/HomeController.java
+++ SpringBootweb1/SpringBootweb1/src/main/java/com/example/SpringBootweb1/HomeController.java
@@ -1,8 +1,12 @@
 package com.example.SpringBootweb1;
 
+import ch.qos.logback.core.model.Model;
 import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpSession;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class HomeController {
@@ -12,12 +16,49 @@ public class HomeController {
         return "index.jsp";
     }
 
+    /*
     @RequestMapping("add")
-    public String add(HttpServletRequest req) {
+    public String add(HttpServletRequest req, HttpSession session) {
         int n1= Integer.parseInt(req.getParameter("num1"));
         int n2= Integer.parseInt(req.getParameter("num2"));
         int result=n1+n2;
         System.out.println(result);
+        session.setAttribute("Sum=",result);
         return "return.jsp";
     }
+    */
+    /*
+    @RequestMapping("add")
+    public String add(@RequestParam("n1") int n1, @RequestParam("n2") int n2, HttpSession session) {
+
+        int result=n1+n2;
+        System.out.println(result);
+        session.setAttribute("Sum=",result);
+        return "return.jsp";
+    }
+*/
+    //Model Object
+/*
+    @RequestMapping("add")
+    public String add(@RequestParam("n1") int n1, @RequestParam("n2") int n2, Model model) {
+        int result=n1+n2;
+        System.out.println(result);
+        model.addText("Sum="+result);
+        return "return";
+    }
+*/
+
+    @RequestMapping("add")
+    public ModelAndView add(@RequestParam("n1") int n1, @RequestParam("n2") int n2, ModelAndView mv) {
+        int result=n1+n2;
+        System.out.println(result);
+
+        mv.addObject("result",result);
+        mv.setViewName("result");
+        return mv;
+
+    }
+
+
+
 }
